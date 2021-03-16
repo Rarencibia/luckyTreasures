b1904e425e860a06d3e86d0c50d5effe73c913da
Rarencibia <40923510+Rarencibia@users.noreply.github.com>
2021-03-16 09:56:25 -0400

    Massive changes to slideshow carousel on homepage. Complete overhaul. Slightly updated resizing on about Us page and made background a bit darker for readability purposes.

--- public/javascripts/carousel.js
+++ public/javascripts/carousel.js
@@ -1,47 +1,107 @@
-async function getJson(){
-    let response = await fetch('./javascripts/carousel.json');
-    let data = await response.json();
+// async function getJson(){
+//     let response = await fetch('./javascripts/carousel.json');
+//     let data = await response.json();
 
-    let carouselWrapper = document.getElementById("carouselWrapper");
+//     let carouselWrapper = document.getElementById("carouselWrapper");
 
-    let leftArrow = document.getElementById("leftArrow");
-    let rightArrow = document.getElementById("rightArrow");
-
-
-    function sleep(ms) {
-        return new Promise(resolve => setTimeout(resolve, ms));
-      }
+//     let leftArrow = document.getElementById("leftArrow");
+//     let rightArrow = document.getElementById("rightArrow");
 
 
+//     let index = 0 ;
+
+//     function carouselFunctionality(x) {
+//         if(x == 0){
+//             let img = document.createElement('img');
+//             img.src = data.salesImage[0].image;
+//             img.setAttribute("id", "carouselImg");
+
+//             let h1Header = document.createElement("h1");
+//             h1Header.innerHTML = data.salesImage[0].title;
+//             h1Header.setAttribute("id", "carouselHeader")
+
+//             carouselWrapper.replaceChild(img);
+//             carouselWrapper.replaceChild(h1Header)
 
 
-    for(let i = 0; i < data.salesImage.length; i++){
-        let img = document.createElement('img');
-            img.src = data.salesImage[i].image;
-            img.setAttribute("id", "carouselImg");
+//         }else if(x == 1) { 
+//             let img = document.createElement('img');
+//             img.src = data.salesImage[1].image;
+//             img.setAttribute("id", "carouselImg");
 
-            let h1Header = document.createElement("h1");
-            h1Header.innerHTML = data.salesImage[i].title;
-            h1Header.setAttribute("id", "carouselHeader")
+//             let h1Header = document.createElement("h1");
+//             h1Header.innerHTML = data.salesImage[1].title;
+//             h1Header.setAttribute("id", "carouselHeader")
 
-            carouselWrapper.appendChild(img);
-            carouselWrapper.appendChild(h1Header);
+//             carouselWrapper.replaceChild(img);
+//             carouselWrapper.replaceChild(h1Header)
 
-            await sleep(3000);
 
-            carouselWrapper.removeChild(img);
-            carouselWrapper.removeChild(h1Header);
+//         }else if(x == 2){
+//             console.log(data.salesImage[2])
+//         }
+//     }
 
-            if(i == 2){
-                i = i - 3;
-            }
-        }
+
+
+//     // window.onload = carouselLoop()
+
+
+//     leftArrow.addEventListener("click", () => {
+//         index -= 1;
+//         carouselFunctionality(index);
+//     });
+
+//     rightArrow.addEventListener("click", () => {
+//         index += 1;
+//         carouselFunctionality(index);
+//     });
+
+//     carouselFunctionality(index);
+//     // function sleep(ms) {
+//     //     return new Promise(resolve => setTimeout(resolve, ms));
+//     //   }
+
+
+
+//     //   async function carouselLoop(){
+//         // while(running){
+
+//         //     for(let i = 0; i < data.salesImage.length; i++){
+//             // let img = document.createElement('img');
+//             //     img.src = data.salesImage[i].image;
+//             //     img.setAttribute("id", "carouselImg");
+    
+//             //     let h1Header = document.createElement("h1");
+//             //     h1Header.innerHTML = data.salesImage[i].title;
+//             //     h1Header.setAttribute("id", "carouselHeader")
+    
+//                 // carouselWrapper.appendChild(img);
+//                 // carouselWrapper.appendChild(h1Header);
+    
+//         //         await sleep(3000);
+    
+//                 // carouselWrapper.removeChild(img);
+//                 // carouselWrapper.removeChild(h1Header);
+    
+//         //         if(i == 2){
+//         //             i = i - 3;
+//         //         }
+//         //     }
+//         // }
+//     //   }
+
+
+
+
+
+
+
     
 
-    
 
 
+// }
 
-}
 
-getJson();
+// getJson();
