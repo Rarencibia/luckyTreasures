e2db507c30dc045c894b3b221ad8adcb10fa8d50
Rarencibia <40923510+Rarencibia@users.noreply.github.com>
2021-04-10 19:29:04 -0400

    major updates to UI. added widgets and necca figurines section

---
+++ public/javascripts/linkIconImage.js
@@ -0,0 +1,8 @@
+let linkIconImg = document.getElementsByClassName('linkIconImg');
+
+
+for(let i = 0; i < linkIconImg.length; i++){
+    linkIconImg[i].addEventListener('click', ()=>{
+        
+    })
+}
